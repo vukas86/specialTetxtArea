94e4c73136c809b44aa68d05be03ea130dee99f3
“vukas86” <vukasbit@gmail.com>
2022-01-08 15:39:17 +0100

    minor js changes
    removed extra js codes

--- scrypt.js
+++ scrypt.js
@@ -5,9 +5,7 @@ const progressElement = document.querySelector("#file");
 const buttonElement = document.querySelector("#btn");
 const containerElement = document.querySelector(".container");
 const msgElement = document.createElement("p");
-const msgSpanElement = document.createElement("span");
 msgElement.classList.add("text");
-msgSpanElement.classList.add("spanText");
 
 textAreaElement.addEventListener("keydown", count);
 buttonElement.addEventListener("click", displayMessage);
@@ -15,10 +13,13 @@ buttonElement.addEventListener("click", displayMessage);
 function count(e) {
   let num = textAreaElement.value.length;
   progressElement.setAttribute("value", `${num}`);
+
   if (num > 30) {
     warningElement.classList.remove("hidden");
     warningElement.textContent = `${num}/40 characters left`;
   }
+  if (num < 30) warningElement.classList.add("hidden");
+
   if (num >= 40) {
     errorElement.classList.remove("hidden");
     warningElement.classList.add("hidden");
@@ -32,13 +33,11 @@ function count(e) {
     buttonElement.style.background = "";
     errorElement.classList.add("hidden");
   }
-  if (num < 30) warningElement.classList.add("hidden");
 }
 
 function displayMessage(e) {
   e.preventDefault();
   containerElement.style.visibility = "hidden";
   msgElement.textContent = `Your message: "${textAreaElement.value}" is succesfully submited!`;
-  console.log(msgElement);
   document.body.prepend(msgElement);
 }
